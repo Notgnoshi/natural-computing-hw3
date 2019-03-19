f47f9d58c934e26de156573c14d8fbb6c33262da
Austin Gill <notgnoshi@gmail.com>
2019-03-19 12:29:37 -0600

    oopify the ACA

--- natural/ants/__init__.py
+++ natural/ants/__init__.py
@@ -1,4 +1,5 @@
 """Implements the ant clustering algorithm."""
-from .random import init_ants, init_grid
-from .plotting import plot_grid
 from .constants import *
+from .clustering import AntClustering
+from .plotting import plot_grid
+from .random import init_ants, init_grid
--- natural/ants/clustering.py
+++ natural/ants/clustering.py
@@ -1,45 +1,53 @@
-def perceived_fraction(grid, cell, neighborhood):
-    """Get the perceived fraction of objects around the given cell.
+class AntClustering:
+    """Implements the Ant Clustering Algorithm."""
 
-    :param grid: The grid to search through
-    :param cell: An (x, y) tuple of coordinates of the cell to consider
-    :param neighborhood: The size of the square neighborhood around the cell.
-    """
-    raise NotImplementedError
+    def __init__(self, grid):
+        """Initialize the algorithm to cluster the given grid.
 
+        :param grid: The grid to cluster.
+        :type grid: A 2D array of integers.
+        """
+        # Perform a copy so that we can modify the given grid willy-nilly.
+        self.grid = grid.copy()
 
-def pickup_probability(grid, cell, f, k1):
-    """Determine the probability of an ant picking up the object in the given cell.
+    def perceived_fraction(self, cell, neighborhood):
+        """Determine the perceived fraction of objects around the given cell.
 
-    :param grid: The grid of objects.
-    :param cell: An (x, y) tuple of coordinates of the cell to consider.
-    :param f: The perceived fraction of objects near the given cell.
-    :param k1: A tunable parameter.
-    """
-    raise NotImplementedError
+        :param cell: An (x, y) tuple of coordinates of the cell to consider.
+        :param neighborhood: The size of the square neighborhood around the cell.
+        """
+        raise NotImplementedError
 
+    def pickup_probability(self, cell, f, k1):
+        """Determine the probability of an ant picking up the object in the given cell.
 
-def dropoff_probability(grid, cell, f, k2):
-    """Determine the probability of an ant dropping off its load in the given cell.
+        :param cell: An (x, y) tuple of coordinates of the cell to consider.
+        :param f: The perceived fraction of objects near the given cell.
+        :param k1: A tunable parameter.
+        """
+        raise NotImplementedError
 
-    :param grid: The grid of objects.
-    :param cell: An (x, y) tuple of coordinates of the cell to consider.
-    :param f: The perceived fraction of objects near the given cell.
-    :param k2: A tunable parameter.
-    """
-    raise NotImplementedError
+    def dropoff_probability(self, cell, f, k2):
+        """Determine the probability of an ant dropping off its load in the given cell.
 
+        :param cell: An (x, y) tuple of coordinates of the cell to consider.
+        :param f: The perceived fraction of objects near the given cell.
+        :param k2: A tunable parameter.
+        """
+        raise NotImplementedError
 
-def cluster(grid, iters, ants, neighborhood, k1, k2):
-    """Use the Ant Clustering Algorithm to cluster the given grid.
+    def cluster(self, iters, ants, neighborhood_size, k1, k2):
+        """Cluster the grid with the given tunable parameters.
 
-    :param grid: The grid to cluster.
-    :type grid: A 2D array of RED, BLUE, or UNLOADED values.
-    :param iters: The number of iterations to the the ACA for.
-    :param ants: The number of ants to use.
-    :param neighborhood: The size of the square neighborhood around the cell.
-    :type neighborhood: int
-    :param k1: The tunable parameter for the pickup probability.
-    :param k2: The tunable parameter for the dropoff probability.
-    """
-    raise NotImplementedError
+        Provide the tunable parameters here, so different tunable parameters can
+        be attempted on the same grid.
+
+        :param iters: The number of iterations to the the ACA for.
+        :param ants: The number of ants to use.
+        :param neighborhood: The size of the square neighborhood around the cell.
+        :type neighborhood: int
+        :param k1: The tunable parameter for the pickup probability.
+        :param k2: The tunable parameter for the dropoff probability.
+        :returns: The clustered grid.
+        """
+        raise NotImplementedError
