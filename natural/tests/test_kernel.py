d91f78d8317ac692729176044319ff4070132853
Austin Gill <notgnoshi@gmail.com>
2019-03-22 19:31:44 -0600

    Use test fixtures to use the same matrix with every test.

--- natural/tests/test_kernel.py
+++ natural/tests/test_kernel.py
@@ -10,6 +10,9 @@ kernel_center = natural.ants.ACA._ACA__kernel_center
 
 
 class KernelTest(unittest.TestCase):
+    def setUp(self):
+        self.m = np.arange(25).reshape((5, 5))
+
     def test_reference(self):
         m = np.zeros((3, 3), dtype=int)
         # Get the upper left 2x2 kernel from the given matrix.
@@ -20,110 +23,100 @@ class KernelTest(unittest.TestCase):
         self.assertEqual(m[0, 0], 1)
 
     def test_same_size(self):
-        m = np.arange(25).reshape((5, 5))
         # Get a kernel the same size as the matrix.
-        k = kernel(m, x1=0, y1=0, x2=4, y2=4)
-        self.assertEqual(m.shape, k.shape)
-        self.assertTrue(np.all(m == k))
+        k = kernel(self.m, x1=0, y1=0, x2=4, y2=4)
+        self.assertEqual(self.m.shape, k.shape)
+        self.assertTrue(np.all(self.m == k))
 
     def test_smallest(self):
-        m = np.arange(25).reshape((5, 5))
         # Get the smallest possible kernel.
-        k = kernel(m, x1=0, y1=0, x2=0, y2=0)
+        k = kernel(self.m, x1=0, y1=0, x2=0, y2=0)
         self.assertEqual(k.shape, (1, 1))
-        self.assertEqual(k[0, 0], m[0, 0])
-        self.assertEqual(k[-1, -1], m[0, 0])
+        self.assertEqual(k[0, 0], self.m[0, 0])
+        self.assertEqual(k[-1, -1], self.m[0, 0])
 
     def test_contained(self):
-        m = np.arange(25).reshape((5, 5))
         # Get a kernel fully contained within the matrix.
-        k = kernel(m, x1=1, y1=1, x2=3, y2=3)
+        k = kernel(self.m, x1=1, y1=1, x2=3, y2=3)
         self.assertEqual(k.shape, (3, 3))
-        self.assertEqual(k[0, 0], m[1, 1])
-        self.assertEqual(k[-1, -1], m[3, 3])
+        self.assertEqual(k[0, 0], self.m[1, 1])
+        self.assertEqual(k[-1, -1], self.m[3, 3])
 
     def test_top(self):
-        m = np.arange(25).reshape((5, 5))
         # Hang off the top of the matrix
-        k = kernel(m, x1=0, y1=-2, x2=2, y2=2)
+        k = kernel(self.m, x1=0, y1=-2, x2=2, y2=2)
         self.assertEqual(k.shape, (3, 3))
-        self.assertEqual(k[0, 0], m[0, 0])
-        self.assertEqual(k[-1, -1], m[2, 2])
+        self.assertEqual(k[0, 0], self.m[0, 0])
+        self.assertEqual(k[-1, -1], self.m[2, 2])
 
     def test_left(self):
-        m = np.arange(25).reshape((5, 5))
         # Hang off the left of the matrix
-        k = kernel(m, x1=-2, y1=0, x2=2, y2=2)
+        k = kernel(self.m, x1=-2, y1=0, x2=2, y2=2)
         self.assertEqual(k.shape, (3, 3))
-        self.assertEqual(k[0, 0], m[0, 0])
-        self.assertEqual(k[-1, -1], m[2, 2])
+        self.assertEqual(k[0, 0], self.m[0, 0])
+        self.assertEqual(k[-1, -1], self.m[2, 2])
 
     def test_bottom(self):
-        m = np.arange(25).reshape((5, 5))
         # Hang off the bottom of the matrix
-        k = kernel(m, x1=0, y1=4, x2=1, y2=5)
+        k = kernel(self.m, x1=0, y1=4, x2=1, y2=5)
         self.assertEqual(k.shape, (2, 1))
-        self.assertEqual(k[0, 0], m[0, 4])
-        self.assertEqual(k[-1, -1], m[1, 4])
+        self.assertEqual(k[0, 0], self.m[0, 4])
+        self.assertEqual(k[-1, -1], self.m[1, 4])
 
     def test_right(self):
-        m = np.arange(25).reshape((5, 5))
         # Hang off the right of the matrix
-        k = kernel(m, x1=3, y1=0, x2=6, y2=2)
+        k = kernel(self.m, x1=3, y1=0, x2=6, y2=2)
         self.assertEqual(k.shape, (2, 3))
-        self.assertEqual(k[0, 0], m[3, 0])
-        self.assertEqual(k[-1, -1], m[4, 2])
+        self.assertEqual(k[0, 0], self.m[3, 0])
+        self.assertEqual(k[-1, -1], self.m[4, 2])
 
     def test_combined(self):
-        m = np.arange(25).reshape((5, 5))
-
         # Hang off the top left of the matrix
-        k = kernel(m, x1=-1, y1=-1, x2=2, y2=2)
+        k = kernel(self.m, x1=-1, y1=-1, x2=2, y2=2)
         self.assertEqual(k.shape, (3, 3))
-        self.assertEqual(k[0, 0], m[0, 0])
-        self.assertEqual(k[-1, -1], m[2, 2])
+        self.assertEqual(k[0, 0], self.m[0, 0])
+        self.assertEqual(k[-1, -1], self.m[2, 2])
 
         # Hang off the bottom right of the matrix
-        k = kernel(m, x1=3, y1=3, x2=5, y2=5)
+        k = kernel(self.m, x1=3, y1=3, x2=5, y2=5)
         self.assertEqual(k.shape, (2, 2))
-        self.assertEqual(k[0, 0], m[3, 3])
-        self.assertEqual(k[-1, -1], m[4, 4])
+        self.assertEqual(k[0, 0], self.m[3, 3])
+        self.assertEqual(k[-1, -1], self.m[4, 4])
+
 
 class KernelCenterTest(unittest.TestCase):
+    def setUp(self):
+        self.m = np.arange(25).reshape((5, 5))
+
     def test_contained(self):
-        m = np.arange(25).reshape((5, 5))
-        k = kernel_center(m, x=2, y=2, r=1)
+        k = kernel_center(self.m, x=2, y=2, r=1)
 
         self.assertEqual(k.shape, (3, 3))
-        self.assertEqual(k[0, 0], m[1, 1])
-        self.assertEqual(k[-1, -1], m[3, 3])
+        self.assertEqual(k[0, 0], self.m[1, 1])
+        self.assertEqual(k[-1, -1], self.m[3, 3])
 
     def test_contains(self):
-        m = np.arange(25).reshape((5, 5))
-        k = kernel_center(m, x=2, y=2, r=3)
-        self.assertEqual(k.shape, m.shape)
-        self.assertTrue(np.all(k == m))
+        k = kernel_center(self.m, x=2, y=2, r=3)
+        self.assertEqual(k.shape, self.m.shape)
+        self.assertTrue(np.all(k == self.m))
 
-        k = kernel_center(m, x=2, y=2, r=4)
-        self.assertEqual(k.shape, m.shape)
-        self.assertTrue(np.all(k == m))
+        k = kernel_center(self.m, x=2, y=2, r=4)
+        self.assertEqual(k.shape, self.m.shape)
+        self.assertTrue(np.all(k == self.m))
 
     def test_left(self):
-        m = np.arange(25).reshape((5, 5))
-        k = kernel_center(m, x=0, y=0, r=1)
+        k = kernel_center(self.m, x=0, y=0, r=1)
         self.assertEqual(k.shape, (2, 2))
-        self.assertEqual(k[0, 0], m[0, 0])
-        self.assertEqual(k[-1, -1], m[1, 1])
+        self.assertEqual(k[0, 0], self.m[0, 0])
+        self.assertEqual(k[-1, -1], self.m[1, 1])
 
     def test_right(self):
-        m = np.arange(25).reshape((5, 5))
-        k = kernel_center(m, x=4, y=4, r=1)
+        k = kernel_center(self.m, x=4, y=4, r=1)
         self.assertEqual(k.shape, (2, 2))
-        self.assertEqual(k[0, 0], m[3, 3])
-        self.assertEqual(k[-1, -1], m[4, 4])
+        self.assertEqual(k[0, 0], self.m[3, 3])
+        self.assertEqual(k[-1, -1], self.m[4, 4])
 
     def test_smallest(self):
-        m = np.arange(25).reshape((5, 5))
-        k = kernel_center(m, x=2, y=2, r=0)
+        k = kernel_center(self.m, x=2, y=2, r=0)
         self.assertEqual(k.shape, (1, 1))
-        self.assertEqual(k[0, 0], m[2, 2])
+        self.assertEqual(k[0, 0], self.m[2, 2])
--- natural/tests/test_probability.py
+++ natural/tests/test_probability.py
@@ -2,55 +2,54 @@ import unittest
 
 import numpy as np
 
-import natural.ants
 from natural.ants import Ant
 
-# Name mangling == private members. Didn't you know?
-perceived_fraction = natural.ants.Ant.perceived_fraction
-k1 = 0.5
-k2 = 0.5
-ant = Ant(5, 5, k1, k2)
 
 class ProbabilityFunctionTest(unittest.TestCase):
+    def setUp(self):
+        # Called before each test method.
+        k1 = 0.5
+        k2 = 0.5
+        self.ant = Ant(5, 5, k1, k2)
+
     def test_all_colors(self):
         # Kernel with no colors
-        kernel = np.zeros((5,5), dtype=int)
+        kernel = np.zeros((5, 5), dtype=int)
 
-        kernel[0,:] = 2
-        kernel[:,4] = 3
-        kernel[4,:] = 3
-        kernel[1,:] = 4
+        kernel[0, :] = 2
+        kernel[:, 4] = 3
+        kernel[4, :] = 3
+        kernel[1, :] = 4
 
-        self.assertEqual(ant.perceived_fraction(kernel, 1), 0)
-        self.assertEqual(ant.perceived_fraction(kernel, 2), (4/25))
-        self.assertEqual(ant.perceived_fraction(kernel, 3), (8/25))
-        self.assertEqual(ant.perceived_fraction(kernel, 4), (5/25))
+        self.assertEqual(self.ant.perceived_fraction(kernel, 1), 0)
+        self.assertEqual(self.ant.perceived_fraction(kernel, 2), (4 / 25))
+        self.assertEqual(self.ant.perceived_fraction(kernel, 3), (8 / 25))
+        self.assertEqual(self.ant.perceived_fraction(kernel, 4), (5 / 25))
 
     def test_pickup_probability_high(self):
-        kernel = np.zeros((5,5), dtype=int)
+        kernel = np.zeros((5, 5), dtype=int)
         kernel[0, 0] = 1
 
-        f = ant.perceived_fraction(kernel, 1)
-        self.assertAlmostEqual(ant.pickup_probability(f), 0.8573388203017831)
+        f = self.ant.perceived_fraction(kernel, 1)
+        self.assertAlmostEqual(self.ant.pickup_probability(f), 0.8573388203017831)
 
     def test_pickup_probability_low(self):
-        kernel = np.ones((5,5), dtype=int)
+        kernel = np.ones((5, 5), dtype=int)
         kernel[0, 0] = 0
 
-        f = ant.perceived_fraction(kernel, 1)
-        self.assertAlmostEqual(ant.pickup_probability(f), 0.11728279226871832)
+        f = self.ant.perceived_fraction(kernel, 1)
+        self.assertAlmostEqual(self.ant.pickup_probability(f), 0.11728279226871832)
 
     def test_dropoff_probabilty_low(self):
-        kernel = np.zeros((5,5), dtype=int)
+        kernel = np.zeros((5, 5), dtype=int)
         kernel[0, 0] = 1
-        
-        f = ant.perceived_fraction(kernel, 1)
-        self.assertAlmostEqual(ant.dropoff_probability(f), (1/25)*2)
+
+        f = self.ant.perceived_fraction(kernel, 1)
+        self.assertAlmostEqual(self.ant.dropoff_probability(f), (1 / 25) * 2)
 
     def test_dropoff_probabilty_high(self):
-        kernel = np.ones((5,5), dtype=int)
+        kernel = np.ones((5, 5), dtype=int)
         kernel[0, 0] = 0
-        
-        f = ant.perceived_fraction(kernel, 1)
-        self.assertAlmostEqual(ant.dropoff_probability(f), 1)
 
+        f = self.ant.perceived_fraction(kernel, 1)
+        self.assertAlmostEqual(self.ant.dropoff_probability(f), 1)
